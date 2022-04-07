d39a41a8e2b39e3322e828cecbccb10378ffc171
Adrian-99 <adrian.jurek.1109@gmail.com>
2022-04-07 11:15:33 +0200

    Test for /user/authenticate endpoint
    + added new AuthenticateTest

---
+++ test/APITest/UserController/AuthenticateTest.cs
@@ -0,0 +1,82 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Infrastructure.Data;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITest.UserController
+{
+    public class AuthenticateTest : AbstractControllerTest
+    {
+        private static string URL = "/api/user/authenticate";
+        private static string USERNAME = "User1";
+        private static string PASSWORD = "User1!@#";
+
+        protected override void PrepareTestData()
+        {
+            DataSeeder.SeedUsers(databaseContext);
+        }
+
+        [Test]
+        public async Task Authenticate_WithCorrectData()
+        {
+            var authenticateDto = new AuthenticateDto();
+            authenticateDto.Username = USERNAME;
+            authenticateDto.Password = PASSWORD;
+
+            var response = await TestUtils.PostWithJsonBodyAsync(httpClient, URL, authenticateDto);
+            var responseContent = JsonConvert.DeserializeObject<TokensDto>(await response.Content.ReadAsStringAsync());
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(responseContent);
+            Assert.NotNull(responseContent.AccessToken);
+            Assert.NotNull(responseContent.RefreshToken);
+
+            var jwtService = GetService<IJwtService>();
+            Assert.AreEqual(USERNAME, await jwtService.ValidateAccessToken(responseContent.AccessToken));
+            Assert.AreEqual(USERNAME, await jwtService.ValidateRefreshToken(responseContent.RefreshToken));
+        }
+
+        [Test]
+        public async Task Authenticate_WithPasswordToLower()
+        {
+            var authenticateDto = new AuthenticateDto();
+            authenticateDto.Username = USERNAME;
+            authenticateDto.Password = PASSWORD.ToLower();
+
+            var response = await TestUtils.PostWithJsonBodyAsync(httpClient, URL, authenticateDto);
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Authenticate_WithWrongPassword()
+        {
+            var authenticateDto = new AuthenticateDto();
+            authenticateDto.Username = USERNAME;
+            authenticateDto.Password = "Qwerty1@";
+
+            var response = await TestUtils.PostWithJsonBodyAsync(httpClient, URL, authenticateDto);
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Authenticate_WithWrongUsername()
+        {
+            var authenticateDto = new AuthenticateDto();
+            authenticateDto.Username = "randomUser";
+            authenticateDto.Password = PASSWORD;
+
+            var response = await TestUtils.PostWithJsonBodyAsync(httpClient, URL, authenticateDto);
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
